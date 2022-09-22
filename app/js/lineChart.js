71f3c6e9e4c58f499870b41229dd1113196e3b1a
pflofif <bogdanvivchar8@gmail.com>
2022-09-22 17:39:09 +0200

    finish line chart

--- app/js/lineChart.js
+++ app/js/lineChart.js
@@ -1,28 +1,30 @@
-let ctx = document.getElementById('my-chart').getContext('2d');
+let ctxOption = document.getElementById('line-chart');
+ctxOption.height = 240;
 
-let chartData = [17, 26, 23, 32, 29,31,25 ];
+let ctx = ctxOption.getContext('2d');
+let chartData = [17, 26, 23, 32, 29, 31, 25];
 let labels = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
 let chart = new Chart(ctx, {
     type: 'line',
     data: {
         labels: labels,
-        datasets : [{
+        datasets: [{
             borderColor: [
                 "#586fda"
             ],
             data: chartData,
-            lineTension : 0.4
+            lineTension: 0.4
         }]
     },
     options: {
         responsive: true,
         plugins: {
-            title: {
+            /*title: {
                 display: true,
                 text: 'Balance summary',
                 align: 'start',
-                font:{
+                font: {
                     size: 16,
                     weight: '900'
                 },
@@ -31,18 +33,15 @@ let chart = new Chart(ctx, {
                     top: 20,
                     bottom: 10
                 }
-            },
+            },*/
             legend: {
                 display: false
-            },
-            labels: {
-               align: 'top'
             }
         },
         scales: {
             x: {
-                grid:{
-                    display:false
+                grid: {
+                    display: false
                 }
             },
             y: {
@@ -52,5 +51,6 @@ let chart = new Chart(ctx, {
         }
     }
 })
-let balance = chartData.reduce((a,b) => a+b,0);
+
+let balance = chartData.reduce((a, b) => a + b, 0);
 document.getElementById('balance').innerHTML = balance;
